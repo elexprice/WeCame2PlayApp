381967c87ad7cd63914c3563c592e1f2e79d7f32
IE Mixes <iemixes@IEs-MacBook-Pro.local>
2014-04-07 18:55:47 -0500

    Initial Commit

---
+++ WeCame2PlayApp/WC2PAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WC2PAppDelegate.h
+//  WeCame2PlayApp
+//
+//  Created by IE Mixes on 4/7/14.
+//  Copyright (c) 2014 wecame2play.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WC2PAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WeCame2PlayAppTests/WeCame2PlayAppTests.m
@@ -0,0 +1,34 @@
+//
+//  WeCame2PlayAppTests.m
+//  WeCame2PlayAppTests
+//
+//  Created by IE Mixes on 4/7/14.
+//  Copyright (c) 2014 wecame2play.com. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface WeCame2PlayAppTests : XCTestCase
+
+@end
+
+@implementation WeCame2PlayAppTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
